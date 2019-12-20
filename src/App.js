b0a35a9a55bec59e6926e296096b5e6362bb22c5
Darren Dawson <dwdawson@ucsc.edu>
2019-12-20 02:59:32 -0800

    Add <Sidebar/> so user can navigate between pages of website

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+/**
+ * <App/> is the root React component of Taglify frontend.
+ *
+ */
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SpotifyWebAPI from 'spotify-web-api-js';
 
+import Sidebar from './Components/Sidebar/Sidebar.js';
 
-// SPOTIFY API GLOBALS --------------------------------------------------------------------------//
+
+// Global variables for Spotify API
 window.Spotify = null;
 let SpotifyAPI = new SpotifyWebAPI();
+
+
+// =============================================================================
+// <App/>
+// =============================================================================
 
 class App extends React.Component {
 
@@ -18,8 +30,10 @@ class App extends React.Component {
     let token = url_params.access_token;
 
     this.state = {
-      logged_in : token? true : false,
-      token     : token,
+      logged_in : token? true : false,    // true if <App/> can use user's Spotify
+      token     : token,                  // access token given by Spotify for using the API
+
+      currentPage: "untaggedSongs"        // indicates the page the user is currently on
     }
 
     this.playerExistsInterval = null;
@@ -32,10 +46,16 @@ class App extends React.Component {
     }
   }
 
+  // Spotify Web SDK Functions -------------------------------------------------
+  /**
+    * Functions that are necessary for using the Spotify Web SDK
+    *
+  */
+
   getURLParameters() {
     /**
      * Get the parameters from the url and return them as a hashmap object.
-     * 
+     *
      * @returns {object}
      */
     var hashParams = {};
@@ -49,40 +69,41 @@ class App extends React.Component {
     return hashParams;
   }
 
+
   playerExists () {
     /**
      * Checks if the a player has been created through the SpotifyPlayerSDK script included in
      * in public/index.html and initializes it if so.
-     * 
+     *
      * @see index.html
      */
-    
+
     if (window.Spotify !== null) {
       let token = this.state.token;
       window.WebPlayer = new window.Spotify.Player({
         name: 'Taglify Web Player',
         getOAuthToken: cb => { cb(token); }
       });
-  
+
       // Error handling
       window.WebPlayer.addListener('initialization_error', ({ message }) => { console.error(message); });
       window.WebPlayer.addListener('authentication_error', ({ message }) => { console.error(message); });
       window.WebPlayer.addListener('account_error',        ({ message }) => { console.error(message); });
       window.WebPlayer.addListener('playback_error',       ({ message }) => { console.error(message); });
-  
+
       // Playback status updates
       window.WebPlayer.addListener('player_state_changed', state => { console.log(state); });
-  
+
       // Ready
       window.WebPlayer.addListener('ready', ({ device_id }) => {
         console.log('Ready with Device ID', device_id);
       });
-  
+
       // Not Ready
       window.WebPlayer.addListener('not_ready', ({ device_id }) => {
         console.log('Device ID has gone offline', device_id);
       });
-  
+
       // Connect to the player
       window.WebPlayer.connect();
 
@@ -94,7 +115,7 @@ class App extends React.Component {
   createPlayer () {
     if (this.state.logged_in === true) {
       this.playerExistsInterval = setInterval( () => this.playerExists(), 3000 );
-    } 
+    }
   }
 
   logIn () {
@@ -104,13 +125,39 @@ class App extends React.Component {
     window.open('http://localhost:8888', "_self");
   }
 
+  // onClick -------------------------------------------------------------------
+
+
+  onClick_selectPage = (page) => {
+    /**
+     * function selects a page, used by <Sidebar/> for navigation.
+     * page has to be in set [playlists, taggedSongs, untaggedSongs], otherwise function does nothing
+     *
+     * @param   string    page        selects the given page name, assuming it is valid
+     */
+
+     if (["playlists","taggedSongs","untaggedSongs"].indexOf(page) >= 0 && this.state.currentPage !== page) {
+       this.setState({currentPage: page});
+     }
+  }
+
+  // Render --------------------------------------------------------------------
+  /**
+   * Renders the <App/> Component
+   */
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div id="App">
+        <div id="sidebar_container">
+          <Sidebar
+            selectedOption={this.state.currentPage}
+            onClick_selectPage={this.onClick_selectPage}
+          />
+        </div>
+        <div id="page_container">
           <button onClick={() => this.logIn()}>LOGIN</button>
-        </header>
+        </div>
       </div>
     );
   }
---
+++ src/Components/ComponentTemplate.js
@@ -0,0 +1,32 @@
+/*
+ * <[NAME]/> is....
+ */
+
+
+import React, { Component } from 'react';
+import './[NAME].css';
+
+class [NAME] extends Component {
+
+  constructor() {
+    super();
+  }
+
+  // render --------------------------------------------------------------------
+
+  render() {
+    /**
+     * Render function for <[NAME]/>
+     *
+     * @returns HTML for <[NAME]/>
+     */
+
+    return (
+      <div id="[NAME]">
+
+      </div>
+    );
+  }
+}
+
+export default [NAME];
